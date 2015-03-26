a7e392012297875fb30763e384a540b17cffadc2
yann <yann.boisselier@gmail.com>
2015-03-26 12:56:52 +0100

    initial commit

---
+++ sites/all/modules/custom/restful_custom/plugins/restful/RestfulCustomResource.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class RestfulCustomResource extends RestfulEntityBaseNode {
+
+  /**
+   * Overrides RestfulEntityBaseNode::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['body'] = array(
+      'property' => 'body',
+      'sub_property' => 'value',
+    );
+
+    $public_fields['title'] = array(
+      'property' => 'title',
+    );
+
+
+    return $public_fields;
+  }
+}
